1befdacc42df68e5167c5d889dd4ff1bab640c9c
RustyDimes <34825450+RustyDimes@users.noreply.github.com>
2023-06-01 05:43:08 +0400

    auth kinda work kinda fine

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,4 @@
-import { redirect } from '@sveltejs/kit';
-import { linkRoad, user } from '$lib/stores.js';
+import { linkRoad } from '$lib/stores.js';
 let link;
 
 linkRoad.subscribe((value) => {
@@ -15,6 +14,7 @@ export async function handle({ event, resolve }) {
     console.log("IM HOOK");
     const access = event.cookies.get("access");
     const refresh = event.cookies.get("refresh");
+    console.log(access);
     if (!access && !refresh){return await resolve(event);}
 
     const resp =  await fetch(`${link}/api/user`, {
@@ -39,20 +39,30 @@ export async function handle({ event, resolve }) {
         
             if (refresh_reps.ok){
                 const refresh_json = await refresh_reps.json();
+
                 event.cookies.set("access", refresh_json.access, {httpOnly: true, path: '/', maxAge: 60 * 60 * 24});
                 console.log("TOKEN REFRESHED");
-                throw redirect(303, '/profile_data');
+                const resp =  await fetch(`${link}/api/user`, {
+                    method: "GET",
+                    headers:{
+                            'Content-Type':'application/json',
+                                Authorization:`Bearer ${refresh_json.access}`},
+            
+                });
+                if (resp.ok) {
+                    const user_json = await resp.json();
+                    event.locals.user = user_json;   
+                }
+
                 }
             else if (refresh_reps.status === 401){
                 event.cookies.delete('access');
                 event.cookies.delete('refresh');
                 event.locals.user = undefined;
-                throw redirect(302, '/');
             }
 
     }
-
+    
     };
-
-	return await resolve(event);
+    return await resolve(event);
 }
