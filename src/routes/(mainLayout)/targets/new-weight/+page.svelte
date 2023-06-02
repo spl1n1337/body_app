c7eeff4d2c711a5348e81469c52bf4e8660a0348
spl1n1337 <panov1337.lp@gmail.com>
2023-06-02 20:37:33 +0400

    new weight page update

--- src/routes/(mainLayout)/targets/new-weight/+page.svelte
+++ src/routes/(mainLayout)/targets/new-weight/+page.svelte
@@ -5,9 +5,37 @@
     import TrainingHeader from "$lib/components/TrainingHeader.svelte";
     import BackArrow from "$lib/components/BackArrow.svelte";
     import {goto} from '$app/navigation';
+    import { onMount } from "svelte";
+
+    export let data;
+
+    let lastWeighing = data.user.params.weight.slice(-1)[0] || [];
+
+
     let backFunction = ()=> history.back();
 
+    // const horizontalWheelScroll = (node) => {
+    //     console.log(node)
+    //     let isDragging = false;
+    //     let startPosition = null;
+    //     node.addEventListener("touchstart", (evt) => {
+    //     isDragging = true;
+    //     startPosition = evt.touches[0].clientX;
+    //     });
 
+    //     node.addEventListener("touchmove", (evt) => {
+    //     if (isDragging) {
+    //         const currentPosition = evt.touches[0].clientX;
+    //         const distance = currentPosition - startPosition;
+    //         node.scrollLeft -= distance;
+    //         startPosition = currentPosition;
+    //     }
+    //     });
+
+    //     node.addEventListener("touchend", (evt) => {
+    //     isDragging = false;
+    //     });
+    // };
     const horizontalWheelScroll = (node) => {
         let isDragging = false;
         let startPosition = null;
@@ -32,7 +60,11 @@
         isDragging = false;
         });
     };
+    
     const items = Array.from({ length: 1200 });
+
+    onMount(()=>{
+    })
 </script>
 
 <TrainingHeader>
@@ -46,11 +78,11 @@
     <div class="blue-params bg-blue">
         <div class="blue-params-item">
             <div class="params-name text-12s c-white">Предыдущий вес</div>
-            <div class="params-value text-16s-u c-white">56.6</div>
+            <div class="params-value text-16s-u c-white">{lastWeighing.weight ? lastWeighing.weight : '- -'}</div>
         </div>
         <div class="blue-params-item">
             <div class="params-name text-12s c-white">Дата</div>
-            <div class="params-value text-16s-u c-white">20.02.23</div>
+            <div class="params-value text-16s-u c-white">{lastWeighing.timestamp ? lastWeighing.timestamp : '- -'}</div>
         </div>
     </div>
 
@@ -61,7 +93,7 @@
         
 
         <div class="picker"></div>
-        <div class="wheel-element" use:horizontalWheelScroll on:scroll={horizontalWheelScroll}>
+        <div class="wheel-element" style="scroll-behavior: smooth;" use:horizontalWheelScroll on:scroll={() => horizontalWheelScroll}>
             {#each items as item}
             <div class="wheel-item"></div>
             {/each}
@@ -82,35 +114,6 @@
     </div>
 
 
-<div class="footer__nav">
-    <a href="/trainings" class="footer__nav-item">
-        <div class="footer__icon-container">
-            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="24" fill="none" viewBox="0 0 26 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.25 5.25H7a.75.75 0 0 0-.75.75v12c0 .41.34.75.75.75h2.25c.41 0 .75-.34.75-.75V6a.75.75 0 0 0-.75-.75ZM19 5.25h-2.25A.75.75 0 0 0 16 6v12c0 .41.34.75.75.75H19c.41 0 .75-.34.75-.75V6a.75.75 0 0 0-.75-.75ZM19.75 7.5H22a.75.75 0 0 1 .75.75v7.5a.75.75 0 0 1-.75.75h-2.25M6.25 16.5H4a.75.75 0 0 1-.75-.75v-7.5A.75.75 0 0 1 4 7.5h2.25M10 12h6M22.75 12h1.5M1.75 12h1.5"/>
-            </svg>
-        </div>
-        <div class="footer__text text-12s c-dark-gray">Тренировки</div>
-    </a>
-    <a href="/targets" class="footer__nav-item">
-        <div class="footer__icon-container __active">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 12 9-9M18.37 5.63a9.02 9.02 0 1 0 1.75 2.49"/>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.18 8.82a4.5 4.5 0 1 0 1.31 2.93"/>
-            </svg>
-        </div>
-        <div class="footer__text text-12s c-dark-gray c-blue">Цели</div>
-    </a>
-    <a href="/user-profile" class="footer__nav-item">
-        <div class="footer__icon-container">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Z"/>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"/>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.98 18.7a6.75 6.75 0 0 1 12.04 0"/>
-              </svg>
-        </div>
-        <div class="footer__text text-12s c-dark-gray c-dark-gray">Профиль</div>
-    </a>
-</div>
 
 
 <style>
@@ -144,13 +147,14 @@
         right: 20px;
         overflow: hidden;
     }
-    .wheel-element {
+    :global(.wheel-element) {
         min-width: 100vw;
         scroll-snap-type: x mandatory;	
 		display: flex;
 		-webkit-overflow-scrolling: touch;
 		overflow-x: scroll;
         padding: 10vw 0;
+        scroll-behavior: smooth;
     }
     .wheel-item {
         height: 16px;
