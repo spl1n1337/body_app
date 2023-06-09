d15737a29e825368ab55fd720e0a9f0e15f02de4
spl1n1337 <panov1337.lp@gmail.com>
2023-06-10 00:56:57 +0400

    qwdqweewqqweqweeeew

--- src/routes/(mainLayout)/+layout.svelte
+++ src/routes/(mainLayout)/+layout.svelte
@@ -1,14 +1,7 @@
 <script>
 	import NavFooter from '$lib/components/NavFooter.svelte';
 	import { page } from '$app/stores'
-	import { onMount } from 'svelte';
 	$: currentPath = $page.url.pathname;
-	
-	onMount(()=>{
-		document.addEventListener('touchmove', function (event) {
-    	if (event.scale !== 1) { event.preventDefault(); }
-  		}, false);
-	})
 </script>
 
 <slot/>
@@ -18,6 +11,9 @@
 <NavFooter/>
 {/if}
 <style>
+	:global(html) {
+		width: 100% !important;
+	}
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Unbounded:wght@700&display=swap');
 	:global(*) {
 		-webkit-user-select: none; /* Safari */
