34d07fcddfaa2a263f6c1bd7f6c0e6a27b75f892
spl1n1337 <panov1337.lp@gmail.com>
2023-06-10 04:07:56 +0400

    cropper 10

--- src/lib/components/canvasUtils.js
+++ src/lib/components/canvasUtils.js
@@ -27,7 +27,8 @@ export async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
 
-  const safeArea = Math.max(image.width, image.height);
+  const maxSize = Math.max(image.width, image.height)
+  const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2))
 
   // set each dimensions to double largest dimension to allow for a safe area for the
   // image to rotate in without being clipped by canvas context
@@ -42,8 +43,8 @@ export async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
   // draw rotated image and store data.
   ctx.drawImage(
     image,
-    safeArea / 2 - image.width,
-    safeArea / 2 - image.height
+    safeArea / 2 - image.width * 0.5,
+    safeArea / 2 - image.height * 0.5
   )
   const data = ctx.getImageData(0, 0, safeArea, safeArea)
 
@@ -54,54 +55,44 @@ export async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
   // paste generated rotate image with correct offsets for x,y crop values.
   ctx.putImageData(
     data,
-    0 - safeArea / 2 + image.width - pixelCrop.x,
-    0 - safeArea / 2 + image.height - pixelCrop.y
+    Math.round(0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x),
+    Math.round(0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y)
   )
 
-  // return new Promise((resolve) => {
-  //   canvas.toBlob((file) => {
-  //     resolve(URL.createObjectURL(file))
-  //   }, 'image/jpeg')
-  // })
-
   // As Base64 string
-  return canvas.toDataURL('image/jpeg');
+  // return canvas.toDataURL('image/jpeg');
 
   // As a blob
-  // return new Promise((resolve) => {
-  //   canvas.toBlob((file) => {
-  //     console.log(file)
-  //     resolve(URL.createObjectURL(file))
-  //     console.log(resolve)
-  //   }, 'image/jpeg')
-  // })
+  return new Promise((resolve) => {
+    canvas.toBlob((file) => {
+      resolve(URL.createObjectURL(file))
+      console.log(resolve);
+    }, 'image/png')
+  })
 }
 
+export async function getRotatedImage(imageSrc, rotation = 0) {
+  const image = await createImage(imageSrc)
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
 
+  const orientationChanged =
+    rotation === 90 || rotation === -90 || rotation === 270 || rotation === -270
+  if (orientationChanged) {
+    canvas.width = image.height
+    canvas.height = image.width
+  } else {
+    canvas.width = image.width
+    canvas.height = image.height
+  }
 
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.rotate((rotation * Math.PI) / 180)
+  ctx.drawImage(image, -image.width / 2, -image.height / 2)
 
-// export async function getRotatedImage(imageSrc, rotation = 0) {
-//   const image = await createImage(imageSrc)
-//   const canvas = document.createElement('canvas')
-//   const ctx = canvas.getContext('2d')
-
-//   const orientationChanged =
-//     rotation === 90 || rotation === -90 || rotation === 270 || rotation === -270
-//   if (orientationChanged) {
-//     canvas.width = image.height
-//     canvas.height = image.width
-//   } else {
-//     canvas.width = image.width
-//     canvas.height = image.height
-//   }
-
-//   ctx.translate(canvas.width / 2, canvas.height / 2)
-//   ctx.rotate((rotation * Math.PI) / 180)
-//   ctx.drawImage(image, -image.width / 2, -image.height / 2)
-
-//   return new Promise((resolve) => {
-//     canvas.toBlob((file) => {
-//       resolve(URL.createObjectURL(file))
-//     }, 'image/png')
-//   })
-// }
+  return new Promise((resolve) => {
+    canvas.toBlob((file) => {
+      resolve(URL.createObjectURL(file))
+    }, 'image/png')
+  })
+}
