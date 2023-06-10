a41d4dd8e059f25e3fd377cf299398af94f74189
RustyDimes <34825450+RustyDimes@users.noreply.github.com>
2023-06-10 18:29:10 +0400

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,61 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			srcDir: './src',
+			mode: 'development',
+			strategies: 'generateSW' ,
+			scope: '/',
+			base: '/',
+			selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+			manifest: {
+				short_name: 'TopBody',
+				name: 'TopBody',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+			injectManifest: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			devOptions: {
+				enabled: true,
+				suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+				type: 'module',
+				navigateFallback: '/',
+			},
+			// if you have shared info in svelte config file put in a separate module and use it also here
+			kit: {}
+		}
+	)
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
