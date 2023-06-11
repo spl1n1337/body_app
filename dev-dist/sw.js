9e47aa208587479ebf233b21d6f10769a19b8194
spl1n1337 <panov1337.lp@gmail.com>
2023-06-12 00:12:59 +0400

    delete navigator

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fc5f5acf'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.j7tdr731h7g"
+    "revision": "0.86hea9eqjio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
